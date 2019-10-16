50341edc05ba36f1299868ab2d4635952e9c9eb3
naeemanwarphenologix <naeem.anwar@phenologix.com>
2019-10-16 21:23:03 +0500

    InitalCommit

---
+++ DatingApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly DataContext _dataContext;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, DataContext dataContext)
+        {
+            _logger = logger;
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var values = _dataContext.Values.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            var value = _dataContext.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(value);
+        }
+    }
+}
---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext :DbContext
+    {
+        /// <summary>
+        /// powe shell commands for migarations are
+        /// enable migration
+        /// add-migration initialcreate
+        /// update-database 
+        /// </summary>
+        /// <param name="dbContextOptions"></param>
+        public DataContext(DbContextOptions<DataContext> dbContextOptions):base(dbContextOptions){}
+        public DbSet<Value> Values { get; set; }
+    }
+}
